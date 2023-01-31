551faec3e03e58dbf9bebe8566e3088945ab0523
kkyesyes <3505222629@qq.com>
2023-01-31 19:50:24 +0800

    homework_230131 commit

--- home.c
+++ home.c
@@ -1,28 +1,54 @@
-
-
+/////////////////////////////////////////////////
+//algorithm file                               //
+//homework_230131                              //
+/////////////////////////////////////////////////
 #include <stdio.h>
-#include <string.h>
 
 int main() {
-    int arr[7][10] = {{0,0,0,0,0,0,0,0,0,0}, {0,1,0,0,0,0,0,0,0,0}, {0,1,1,0,0,0,0,0,0,0}};
+    /*变量初始化0*/
+    int a1 = 0;
+    int a2 = 0;
+    int n = 0;
+    int arr[120] = {0};
 
-    for (int i = 3; i <= 6; ++i) {
-        for (int j = 1; j <= (i + 1); ++j) {
-            arr[i][j] = arr[i - 1][j - 1] + arr[i - 1][j];
-        }
-    }    
+    /*自定义赋值*/
+    scanf("%d %d %d", &a1, &a2, &n);
 
-    int space = 13;
+    /*数组初始化*/
+    arr[0] = a1;
+    arr[1] = a2;
 
-    for (int i = 1; i <= 6; ++i) {
-        for (int j = 0; j < space; ++j) {
-            printf(" ");
+    /*循环计算填入*/
+    int j = 0;
+    int temp = 2;
+    int multi = 0;  //乘积初始化0
+    for (int i = 2; i < n; ++i) {
+        multi = arr[i - 1] * arr[i - 2];  //乘积计算
+        for (int j = temp; ; ++j) {  //检测空项填入
+            if (arr[j] == 0) {
+                if ( (multi / 10) == 0) {  //结果为一位数时赋值
+                    arr[j] = multi;
+                    temp = ++j;
+                    break;
+                }
+                else {  //结果为两位数时赋值
+                    arr[j] = multi / 10;
+                    arr[j + 1] = multi % 10;
+                    temp = j + 2;
+                    break;
+                }
+            }
         }
-        for (int k = 1; k <= i; ++k) {
-            printf("%-4d", arr[i][k]);
+    }
+
+    /*打印输出*/
+    for (int i = 0; i < n; ++i) {
+        if (i !=  (n - 1) ) {
+            printf("%d ", arr[i]);
         }
-        printf("\n");
-        space -= 2;
+        else {
+            printf("%d", arr[i]);  //行末无空格
+        }
     }
 
     return 0;
