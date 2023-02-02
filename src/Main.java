e560f5b368e87397fdc278f58f5ea223c561929b
kkyesyes <3505222629@qq.com>
2023-02-02 15:16:13 +0800

    sort algorithm 230201commit

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,27 @@ public class Main {
     public static void main(String[] args) {
         Scanner myScanner = new Scanner(System.in);
 
-        String name = myScanner.next();
-        int age = myScanner.nextInt();
-        int salary = myScanner.nextInt();
+        int arr[] = new int[15];
 
-        System.out.println("Your name is:" + name);
-        System.out.println("Your age is:" + age);
-        System.out.println("Your salary is:" + salary);
+        System.out.println("你要输入几个数字来进行排序：(<15)");
+        int n = myScanner.nextInt();
+
+        for (int i = 0; i < n; ++i) {
+            arr[i] = myScanner.nextInt();
+        }
+
+        for (int i = 0; i < (n - 1); ++i) {
+            for (int j = 0; j < (n - i - 1); ++j) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+
+        for (int i = 0; i < n; ++i) {
+            System.out.print(arr[i] + " ");
+        }
     }
 }
